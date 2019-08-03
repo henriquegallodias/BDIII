832b212d4740cbd0bfb1486e99fc64aca5586df1
henriquegallodias <henrique0521@hotmail.com>
2019-08-03 12:00:05 -0300

    PRIMEIRA AULA DE SQL SERVER, COMANDO GO, DROP E ETC

---
+++ SQLQuery1.sql
@@ -0,0 +1,53 @@
+USE master
+
+GO
+
+DROP DATABASE info_loja
+
+GO
+
+CREATE DATABASE info_loja
+
+GO
+
+USE info_loja
+
+GO
+
+create table tb_clientes(
+       id_cliente int PRIMARY KEY IDENTITY(1,1),
+       nome nvarchar(50) not null,
+       endereco nvarchar(100),
+       idade int NOT NULL,
+       sexo char(1) NOT NULL,
+       fone nvarchar(15),
+       email nvarchar(70),
+)
+GO
+
+create table tb_hardware(
+       id_hardware int PRIMARY KEY IDENTITY(1,1),
+       descricao nvarchar(50) not null,
+       preco_unit decimal NOT NULL,
+       qtde_atual int NOT NULL, --0 caso nao tenha no estoque
+       qtde_minima int,
+       img image DEFAULT NULL
+)
+GO
+
+create table tb_vendas(
+       id_venda int primary key IDENTITY(1,1),
+       id_cliente int not null,
+       data date not null,
+       desconto decimal(2,2)
+)
+GO
+
+create table tb_vendas_itens(
+       id_item int PRIMARY KEY identity(1,1),
+       id_venda int not null,
+       id_hardware int not null,
+       qtde_item int not null,
+       pco_vda decimal(8,2) not null
+)
+GO
