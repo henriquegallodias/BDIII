218cab0c374c0b659452f40403b132c2b1008d77
henriquegallodias <henrique0521@hotmail.com>
2019-08-25 21:43:13 -0300

    criadas as tabelas de vendas(vendidas e canceladas)

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,66 +2,61 @@ USE master
 
 GO
 
-DROP DATABASE info_loja
+DROP DATABASE info_store
 
 GO
 
-CREATE DATABASE info_loja
+CREATE DATABASE info_store
 
 GO
 
-USE info_loja
+USE info_store
 
 GO
 
-create table tb_clientes(
-       id_cliente int PRIMARY KEY IDENTITY(1,1),
-       nome nvarchar(50) not null,
-       endereco nvarchar(100),
-       idade int NOT NULL,
-       sexo char(1) NOT NULL,
-       fone nvarchar(15),
-       email nvarchar(70),
+CREATE TABLE tb_clientes(
+id_cliente int PRIMARY KEY IDENTITY(1,1),
+nome VARCHAR(60) NOT NULL,
+endereco VARCHAR(120) NOT NULL,
+idade INT NOT NULL,
+sexo CHAR(1) NOT NULL,
+fone VARCHAR(15),
+email VARCHAR(70)
 )
 GO
 
-create table tb_hardware(
-       id_hardware int PRIMARY KEY IDENTITY(1,1),
-       descricao nvarchar(50) not null,
-       preco_unit decimal NOT NULL,
-       qtde_atual int NOT NULL, --0 caso nao tenha no estoque
-       qtde_minima int,
-       img image DEFAULT NULL
+CREATE TABLE tb_produtos(
+id_produto INT PRIMARY KEY IDENTITY(1,1),
+nome VARCHAR(30) NOT NULL,
+detalhes VARCHAR(30) NOT NULL,
+data date NOT NULL,
+desconto DECIMAL(2,2)
 )
 GO
 
-create table tb_vendas(
-       id_venda int primary key IDENTITY(1,1),
-       id_cliente int not null,
-       data date not null,
-       desconto decimal(2,2)
+CREATE TABLE tb_vendas(
+id_venda INT PRIMARY KEY IDENTITY(1,1),
+id_cliente INT NOT NULL FOREIGN KEY REFERENCES tb_clientes(id_cliente),
+id_produto INT NOT NULL FOREIGN KEY REFERENCES tb_produtos(id_produto),
+data date NOT NULL,
+desconto DECIMAL(2,2),
 )
 GO
 
-CREATE TABLE tb_itens_vendidos(
-id_item_vendido INT PRIMARY KEY IDENTITY(1,1),
-id_venda INT NOT NULL FOREIGN KEY REFERENCES tb_vendas(id_venda),
-id_harware INT NOT NULL FOREIGN KEY REFERENCES tb_hardware(id_hardware),
-)
-GO
-
-create table tb_vendas_itens(
-       id_item int PRIMARY KEY identity(1,1),
-       id_venda int not null,
-       id_hardware int not null,
-       qtde_item int not null,
-       pco_vda decimal(8,2) not null
+CREATE TABLE tb_vendas_sucedidas(
+id_item_venda_sucedida INT PRIMARY KEY IDENTITY(1,1),
+id_venda INT NOT NULL UNIQUE FOREIGN KEY REFERENCES tb_vendas(id_venda),
 )
 GO
 
 CREATE TABLE tb_vendas_canceladas(
-id_vendas_canceladas INT PRIMARY KEY IDENTITY(1,1),
-id_item_vendido INT NOT NULL FOREIGN KEY REFERENCES tb_itens_vendidos(id_item_vendido)
+id_venda_cancelada INT PRIMARY KEY IDENTITY(1,1),
+id_item_cancelado INT NOT NULL UNIQUE FOREIGN KEY REFERENCES tb_vendas(id_venda)
 )
+GO
+
+INSERT INTO tb_clientes (nome,endereco,idade,sexo,fone,email) VALUES ('Henrique','Rua One',17,'M','11988387640','henrique@gmail.com');
 
 GO
+
+INSERT INTO tb_produtos (nome,detalhes,data,desconto) VALUES ('Teclado Asus Wi-Fi','Preto','17/01/2002',0.1);
