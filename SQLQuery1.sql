2539fc6b54f4b5f300e9ee0fe2478084e14ecaf1
henriquegallodias <henrique0521@hotmail.com>
2019-10-31 20:18:41 -0300

    store procedure de desconto

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -148,3 +148,47 @@ on p.id_produto = vi.id_produto WHERE vi.id_produto IS NULL
  SELECT * FROM tb_clientes_auditoria
  
  GO
+ 
+IF OBJECT_ID ('dbo.select_produtos_por_faixa_de_desconto') 
+IS NOT NULL 
+DROP PROCEDURE dbo.select_produtos_por_faixa_de_desconto 
+
+GO 
+
+IF OBJECT_ID ('dbo.select_produtos_por_faixa_de_desconto') IS NOT NULL 
+DROP PROCEDURE dbo.aplica_desconto_no_preco
+
+GO  
+          
+CREATE PROCEDURE dbo.select_produtos_por_faixa_de_desconto( @desc_min as decimal(10,2), @desc_max as decimal(10,2) ) 
+AS BEGIN  
+select @desc_min as "Desconto Minimo", @desc_max as "Desconto Maximo" 
+END
+
+GO
+
+DECLARE @DescontoMinimo decimal(5,2); DECLARE @MaximoDeDesconto decimal(5,2);
+SET  @DescontoMinimo = 10.5; 
+SET  @MaximoDeDesconto = 20.5;
+Exec dbo.select_produtos_por_faixa_de_desconto @DescontoMinimo,@MaximoDeDesconto Exec dbo.select_produtos_por_faixa_de_desconto @desc_min=@DescontoMinimo,@desc_max=@MaximoDeDesconto
+
+GO
+      
+CREATE PROCEDURE dbo.aplica_desconto_no_preco( @pco as decimal(10,2), @perc_de_desconto as decimal(10,2) = 0.0, @pco_com_desconto as decimal(10,2) OUTPUT ) 
+AS BEGIN  
+SELECT @pco_com_desconto = @pco - (@pco * @perc_de_desconto)
+SELECT @pco_com_desconto as "Preco Com Desconto" 
+END 
+
+GO
+
+DECLARE @Preco decimal(5,2); 
+DECLARE @PercentualDeDesconto decimal(5,2); 
+DECLARE @PrecoComDesconto decimal(5,2); 
+SET  @Preco = 100; 
+SET  @PercentualDeDesconto = 0.5;
+Exec dbo.aplica_desconto_no_preco @Preco,@PercentualDeDesconto,@PrecoComDesconto output PRINT N'mostrando o resultado da procedure' 
+PRINT N'o vlr da variavel de output da procedure foi atribuido a nossa variavel @PrecoComDesconto' 
+SELECT @PrecoComDesconto 
+AS 
+"Vlr da Variavel @PrecoComDesconto"
